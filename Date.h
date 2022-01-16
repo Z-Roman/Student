d7efad18a63a427759dbdb57c80745b63d143b77
Roman <r.zharuk@ukr.net>
2022-01-17 00:29:16 +0200

    Date

---
+++ Date.h
@@ -0,0 +1,16 @@
+#pragma once
+class Date
+{
+	unsigned int day;
+	unsigned int month;
+	unsigned int year;
+
+public:
+
+	Date(unsigned int day = 1, unsigned int month = 1, unsigned int year = 1980)
+	{
+
+	};
+
+};
+
---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student
+{
+	string name;
+	string surname;
+	string patronymic;
+	string adress;
+	string tel;
+
+
+};
